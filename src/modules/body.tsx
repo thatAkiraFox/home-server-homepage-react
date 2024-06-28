1bde1ab7123ceddb56c4ccbbcea8ecbe35663aee
thatAkiraFox <joseph2000@live.it>
2024-06-28 12:14:39 +0200

    'body.tsx' refactor
    Added some internal documentation to the main body component function, added arguments' types for type checking and started working on the header component.
    
    Still need to make up my mind on how to handle nulls or empty string in the service and settings files.

--- src/modules/body.tsx
+++ src/modules/body.tsx
@@ -1,27 +1,66 @@
+import { ReactNode } from "react";
 import services from "../../services.json";
 
-function Button({ name }) {
-  return <button>{name}</button>;
+/**
+ * @throws Error on empty strings for 'logo' or 'url'
+ */
+function Button({
+  name,
+  url,
+  logo,
+}: {
+  name: string;
+  url: string;
+  logo: string;
+}) {
+  if (url === "" || logo === "") {
+    throw new Error(
+      "Make sure that URL and LOGO are NOT NULL OR EMPTY STRINGS"
+    );
+  }
+  return (
+    <div className="button">
+      <img src={logo} />
+      <a href={url}>{name}</a>
+    </div>
+  );
 }
 
-function GroupContainer({ children, name }) {
+function GroupContainer({
+  name,
+  children,
+}: {
+  name: string;
+  children: ReactNode;
+}) {
   return (
     <>
       <div className="container">
-        <label>{name}</label>
+        <h1>{name}</h1>
         {children}
       </div>
     </>
   );
 }
 
+/**
+ * This needs a bit of explaination.
+ *
+ * The 'Array.map' method allows to execute a function on each element of an
+ * array, and our 'services.json' file is basically a huge array of app groups,
+ * with each group containing a name and an array of apps.
+ *
+ * So, first we execute 'map' on the top-level array to iterate through the groups
+ * and create the HTML containers for them and, while doing that, we execute
+ * 'map' on the inner apps array to create the various buttons for each app.
+ */
 export default function Body() {
   return services.map((service) => (
     <GroupContainer
       name={service.name}
       key={service.name}
       children={service.apps.map((app) => (
-        <Button name={app.name} key={app.name} />
+        <Button name={app.name} key={app.name} url={app.url} logo={app.logo} />
       ))}
     />
   ));
--- src/modules/header.tsx
+++ src/modules/header.tsx
@@ -1,7 +1,18 @@
+import settings from "../../settings.json";
+
 export default function Header() {
+  let hostname, logo;
+  if (settings.hostname === null || settings.hostname === "")
+    hostname = "server";
+  if (settings.logo === null || settings.logo === "") logo = "";
+
   return (
-    <>
-      <div className="header"></div>
-    </>
+    <div className="header">
+      <h1>
+        {hostname}
+        <br />
+        "Services Dashboard"
+      </h1>
+    </div>
   );
 }
